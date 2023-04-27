4ec3a4acd54a90c5a2f99a1998f19c15c90a949d
Alvin Ng <alvin.ng@zero1.sg>
2023-04-27 17:10:27 +0800

    mock fetchdata from ChatGpt

--- flipSpringVue/src/frontend/src/components/__tests__/FetchData.spec.js
+++ flipSpringVue/src/frontend/src/components/__tests__/FetchData.spec.js
@@ -1,13 +1,60 @@
-import { describe, it, expect } from 'vitest'
+// from ChatGPT
+import { describe, test, expect, vi } from 'vitest'
+import { shallowMount } from '@vue/test-utils'
+import { ref } from 'vue'
+import FetchData from '@/components/FetchData.vue'
 
-import FetchData from '../FetchData.vue'
-
-describe('test FetchData', () => {
-    it('mount component', async () => {
-        expect(FetchData).toBeTruthy()
+describe('FetchData', () => {
+  test('fetches and displays message', async () => {
+    const mockText = 'Hello world!'
+    global.fetch = vi.fn().mockResolvedValue({
+      text: () => Promise.resolve(mockText)
     })
 
-    it('Really basic test', () => {
-        expect(true).toBe(true);
+    console.log(">>> " + global.fetch())
+
+    const msg = ref(null)
+    const wrapper = shallowMount(FetchData, {
+      props: { msg }
     })
+
+    console.log(">before>> " + wrapper.text())
+
+    // Assert initial state
+    expect(wrapper.find('h3').text()).toBe('msg:')
+
+    // Wait for fetchData to finish
+    await wrapper.vm.$nextTick()
+    console.log(">after>> " + wrapper.text())
+
+    // Assert state after fetchData completes
+    // expect(wrapper.find('h3').text()).toBe(`msg: ${mockText}`)
+  })
 })
+
+// import { describe, it, expect, vi } from 'vitest'
+// import { mount } from '@vue/test-utils'
+// import FetchData from '@/components/FetchData.vue'
+
+// describe('FetchData', () => {
+//   it('fetches and displays message', async () => {
+//     const mockText = 'Hello world!'
+//     global.fetch = vi.fn().mockResolvedValue({
+//       text: () => Promise.resolve(mockText)
+//     })
+
+//     const wrapper = mount(FetchData)
+//     console.log('>:>::>' + wrapper.text())
+
+//     // Assert initial state
+//     expect(wrapper.find('h3').text()).toBe('msg:')
+
+//     // Wait for fetchData to finish
+//     await wrapper.vm.$nextTick()
+
+//     console.log('>:>::>' + wrapper.text())
+
+//     // Assert state after fetchData completes
+//     expect(wrapper.find('h3').text()).toBe(`msg: ${mockText}`)
+//   })
+// })
